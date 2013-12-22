97e8985a0d51b10ef468b03a3146e6c3fa66ae07
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-22 14:55:35 -0800

    GenerateDeploymentStub class

---
+++ app/models/generate_deployment_stub.rb
@@ -0,0 +1,27 @@
+class GenerateDeploymentStub
+  include ActiveModel::Model
+
+  attr_accessor :bosh_director_uuid, :deployment_name
+
+  def generate_stub
+    base_stub.
+      gsub("NAME", deployment_name).
+      gsub("PLACEHOLDER-DIRECTOR-UUID", bosh_director_uuid)
+  end
+
+  def base_stub
+    <<-YAML
+---
+meta:
+  environment: NAME
+  security_groups:
+    - default
+director_uuid: PLACEHOLDER-DIRECTOR-UUID
+releases:
+  - name: etcd
+    version: latest
+properties:
+  etcd: {}
+    YAML
+  end
+end
---
+++ spec/models/generate_deployment_stub_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe GenerateDeploymentStub do
+  let(:service_instance) { instance_double("ServiceInstance") }
+  let(:deployment_uuid_name) { "SOME-UUID-VALUE" }
+  let(:director_uuid) { "director-uuid" }
+
+  subject { GenerateDeploymentStub.new(service_instance) }
+
+  it "creates DeploymentStub" do
+    # uuidtools = class_double("UUIDTools::UUID")
+    # uuidtools.should_receive(:timestamp_create).and_return(deployment_uuid_name)
+
+    stub = GenerateDeploymentStub.new(bosh_director_uuid: director_uuid, deployment_name: deployment_uuid_name).generate_stub
+    expect(stub).to eq(<<-YAML)
+---
+meta:
+  environment: #{deployment_uuid_name}
+  security_groups:
+    - default
+director_uuid: #{director_uuid}
+releases:
+  - name: etcd
+    version: latest
+properties:
+  etcd: {}
+    YAML
+  end
+end
