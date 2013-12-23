52a5d45ddaa47aa16c55669253aa314c38094f1a
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-22 21:56:06 -0800

    Implementation of GenerateDeploymentManifest#generate_manifest

--- app/models/generators/generate_deployment_manifest.rb
+++ app/models/generators/generate_deployment_manifest.rb
@@ -1,10 +1,26 @@
+require 'tempfile'
+
+# Use spiff to apply templates to create a deployment manifest
 class Generators::GenerateDeploymentManifest
   include ActiveModel::Model
 
-  attr_accessor :deployment_stub, :service_plan_stub
+  attr_accessor :service_stub_paths
+  attr_accessor :service_plan_stub, :deployment_stub
 
   def generate_manifest
-    
+    service_plan_stub_file = tempfile('service_plan_stub', service_plan_stub)
+    deployment_stub_file   = tempfile('deployment_stub', deployment_stub)
+    output_file            = tempfile('output')
+
+    input_file_paths = service_stub_paths + [
+      service_plan_stub_file.try(:path),
+      deployment_stub_file.try(:path)
+    ].compact
+    spiff_merge(input_file_paths, output_file.path)
+  ensure
+    service_plan_stub_file.try(:close)
+    deployment_stub_file.try(:close)
+    output_file.try(:close)
   end
 
   def spiff_merge(input_file_paths, output_path)
@@ -15,5 +31,13 @@ class Generators::GenerateDeploymentManifest
 
   def spiff_cmd(args)
     Escape.shell_command(["spiff", *(args.flatten)])
+  end
+
+  # Store contents in a tempfile and return File object
+  # Need to invoke #close when finished
+  def tempfile(name, contents=nil)
+    file = Tempfile.new(name)
+    file.write(contents) unless contents.blank?
+    file
   end
 end
--- spec/models/generators/generate_deployment_manifest_spec.rb
+++ spec/models/generators/generate_deployment_manifest_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
 describe Generators::GenerateDeploymentManifest do
-  subject { Generators::GenerateDeploymentManifest.new }
+  describe "#generate_manifest" do
+    let (:service_stub_paths) { %w[file1.yml file2.yml] }
+    subject { Generators::GenerateDeploymentManifest.new(service_stub_paths: service_stub_paths) }
+
+    it "merges service plan & deployment stubs" do
+      subject.service_plan_stub = "--- {}"
+      subject.deployment_stub = "--- {}"
+      subject.should_receive(:tempfile).with('service_plan_stub', "--- {}").and_return(double(path: "temp1"))
+      subject.should_receive(:tempfile).with('deployment_stub', "--- {}").and_return(double(path: "temp2"))
+      subject.should_receive(:tempfile).with('output').and_return(double(path: "output.yml"))
+      subject.should_receive(:spiff_merge).with(%w[file1.yml file2.yml temp1 temp2], "output.yml")
+      subject.generate_manifest
+    end
+  end
+
   describe "#spiff_merge" do
+    subject { Generators::GenerateDeploymentManifest.new }
     it "merges list of templates into output file" do
       subject.should_receive(:`).with("spiff file1.yml file2.yml > /path/output.yml")
       subject.spiff_merge(%w[file1.yml file2.yml], "/path/output.yml")
