f6f92715bbbb8ff2df40f6d87386c640fd73865c
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-23 14:00:36 -0800

    Document example DeleteServiceInstance usage

--- app/models/actions/delete_service_instance.rb
+++ app/models/actions/delete_service_instance.rb
@@ -1,3 +1,13 @@
+# Created when Broker received requests to delete a service. Performs the action
+# of deleting a BOSH deployment.
+#
+# Example usage:
+#   action = Actions::DeleteServiceInstance.new(
+#     service_id: "b9698740-4810-4dc5-8da6-54581f5108c4",
+#     service_instance_id: "foobar",
+#     deployment_name: "etcd-dcb1c536-6c0b-11e3-9bba-8438354ccefa")
+#   action.save
+#   action.perform
 class Actions::DeleteServiceInstance
   include ActiveModel::Model
 
