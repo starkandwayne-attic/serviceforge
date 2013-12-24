ecd45868ce4624531c1f9dc9de4703a7971f52d1
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-24 13:08:51 -0800

    failing spec for self-plan change URLs

--- spec/requests/cloud_foundry/v2/lifecycle_spec.rb
+++ spec/requests/cloud_foundry/v2/lifecycle_spec.rb
@@ -4,7 +4,9 @@ describe 'the service lifecycle' do
   let(:seed) { RSpec.configuration.seed }
   let(:service_id)   { 'b9698740-4810-4dc5-8da6-54581f5108c4' } # etcd-dedicated-bosh-lite
   let(:service)      { Service.find_by_id(service_id) }
-  let(:plan_id)      { '5cfa57fc-1474-4eb9-9afb' } # 5-server
+  let(:three_server_plan_id)  { '1a448d0e-bc54-4a16-8d2f-ab701be20c40' }
+  let(:five_server_plan_id) { '5cfa57fc-1474-4eb9-9afb' }
+  let(:plan_id)      { five_server_plan_id }
   let(:instance_id)  { "instance-#{seed}" }
   let(:binding_id)   { "binding-#{seed}" }
   let(:app_guid)     { "app-guid-#{seed}" }
@@ -64,7 +66,7 @@ describe 'the service lifecycle' do
     expect(data).to eq({
       'id' => instance_id, 
       'service_id' => service_id,
-      'plan_id' => plan_id,
+      'plan_id' => five_server_plan_id,
       'deployment_name' => deployment_name
     })
 
@@ -91,10 +93,40 @@ describe 'the service lifecycle' do
     expect(response.status).to eq(201)
     instance = JSON.parse(response.body)
 
-    expect(instance.fetch('credentials')).to eq({
+    credentials = instance.fetch('credentials')
+    service_plans = credentials.delete('service_plans')
+    expect(credentials).to eq({
       'hostname' => '10.244.2.6',
       'host' => '10.244.2.6',
       'port' => 4001
+    })
+
+    # 'service_plans' => {
+    #   'help' => 'send PUT to plan URL to trigger change',
+    #   'current' => '5-servers',
+    #   'plans' => {
+    #     '1-server' => { 'url' => 'http://broker-address/apps/v1/services/UUID/change_plan/1-server' },
+    #     '3-servers' => { 'url' => 'http://broker-address/apps/v1/services/UUID/change_plan/3-servers' },
+    #     '5-servers' => '{ 'url' => http://broker-address/apps/v1/services/UUID/change_plan/5-servers' }
+    #   }
+    # }
+    expect(service_plans.fetch('current')).to eq('5-servers') # see let(:plan_id)
+    plans = service_plans.fetch('plans')
+    expect(plans).to be_instance_of(Hash)
+
+    three_server_plan_url = plans.fetch('3-servers').fetch('url')
+    # Trigger downgrade to 1-server plan
+    put URI.parse(three_server_plan_url).path
+
+    ##
+    ## Test the etcd /service_instances entry
+    ##
+    data = JSON.parse($etcd.get("/service_instances/#{instance_id}/model").value)
+    expect(data).to eq({
+      'id' => instance_id, 
+      'service_id' => service_id,
+      'plan_id' => three_server_plan_id,
+      'deployment_name' => deployment_name
     })
 
     ##
