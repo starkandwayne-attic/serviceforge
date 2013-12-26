7945c96da16b4956f0e7ff631b3a7eade6cedc31
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-26 10:16:32 -0800

    BindingCommandClasses have a #perform method invoked

--- app/models/binding_command.rb
+++ app/models/binding_command.rb
@@ -8,11 +8,24 @@ class BindingCommand
   attr_accessor :auth_token
   attr_accessor :service_instance_id, :service_binding_id
 
+  # Which Binding class to invoke and the attributes to pass
+  # to its constructor
+  attr_accessor :klass, :attributes
+
   def save
     $etcd.set("/binding_commands/#{auth_token}/model", to_json)
   end
 
   def destroy
     $etcd.delete("/binding_commands/#{auth_token}/model")
+  end
+
+  def perform
+    binding_command_object.perform
+  end
+
+  private
+  def binding_command_object
+    @binding_command_object ||= Object.const_get(klass).new(attributes)
   end
 end
--- spec/models/binding_command_spec.rb
+++ spec/models/binding_command_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
+class DummyBindingCommand
+  include ActiveModel::Model
+  attr_accessor :some_attribute
+  def perform; end
+end
+
 describe BindingCommand do
-  let(:service)               { instance_double("Service") }
-  let(:service_instance)      { instance_double("ServiceInstance") }
-  let(:service_binding)       { instance_double("ServiceBinding") }
+  let(:service)               { instance_double('Service') }
+  let(:service_instance)      { instance_double('ServiceInstance') }
+  let(:service_binding)       { instance_double('ServiceBinding') }
   let(:service_id)            { 'service-1' }
   let(:service_instance_id)   { 'service-instance-id-1' }
   let(:service_binding_id)    { 'service-binding-id-1' }
@@ -15,7 +21,9 @@ describe BindingCommand do
       command = BindingCommand.new({
         service_instance_id: service_instance_id,
         service_binding_id: service_binding_id,
-        auth_token: auth_token
+        auth_token: auth_token,
+        klass: 'DummyBindingCommand',
+        attributes: { 'some_attribute' => 'somevalue' }
       })
       command.save
 
@@ -23,8 +31,12 @@ describe BindingCommand do
       expect(data).to eq({
         'service_instance_id' => service_instance_id,
         'service_binding_id' => service_binding_id,
-        'auth_token' => auth_token
+        'auth_token' => auth_token,
+        'klass' => 'DummyBindingCommand',
+        'attributes' => { 'some_attribute' => 'somevalue' }
       })
+
+      command.perform
 
       command.destroy
 
