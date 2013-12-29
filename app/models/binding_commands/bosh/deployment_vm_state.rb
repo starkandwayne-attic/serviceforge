409fca81d8ee4bf9b7048113f980cfef04c92345
Dr Nic Williams <drnicwilliams@gmail.com>
2013-12-29 12:11:21 -0800

    Hide vm_cid & agent_id vms_state fields
    These are purely internal to BOSH and no reason to expose them
    to apps/services users.

--- app/models/binding_commands/bosh/deployment_vm_state.rb
+++ app/models/binding_commands/bosh/deployment_vm_state.rb
@@ -1,8 +1,9 @@
 # Example usage:
+#     deployment_name = "etcd-c7525ae2-bb73-4f4c-a654-66ae6f5afc86"
 #     command = BindingCommands::Bosh::DeploymentVmState.new({
 #       service_id: "b9698740-4810-4dc5-8da6-54581f5108c4",
-#       deployment_name: "etcd-X-Y-Z"
-#     }
+#       deployment_name: deployment_name
+#     })
 #     command.perform
 #     command.to_json
 class BindingCommands::Bosh::DeploymentVmState
@@ -15,6 +16,11 @@ class BindingCommands::Bosh::DeploymentVmState
 
   def perform
     @vms_state, @bosh_task_id = bosh_director_client.fetch_vm_state(deployment_name)
+    # Delete internal BOSH IDs from public visibility
+    @vms_state.each do |vm|
+      vm.delete('vm_cid')
+      vm.delete('agent_id')
+    end
   end
 
   def to_json(*)
--- spec/models/binding_commands/bosh/deployment_vm_state_spec.rb
+++ spec/models/binding_commands/bosh/deployment_vm_state_spec.rb
@@ -5,6 +5,10 @@ describe BindingCommands::Bosh::DeploymentVmState do
   let(:deployment_name) { 'deployment-name' }
   let(:bosh_director_client)  { instance_double("Bosh::DirectorClient") }
   let(:vms_state) {
+    [{'job_name'=>'master_job', 'index'=>0, 'ips'=>['10.244.2.6'], 'vm_cid' => 'vm_cid1', 'agent_id' => 'agent_id1'},
+     {'job_name'=>'other_job', 'index'=>0, 'ips'=>['10.244.2.254'], 'vm_cid' => 'vm_cid2', 'agent_id' => 'agent_id1'}]
+  }
+  let(:expected_vms_state) {
     [{'job_name'=>'master_job', 'index'=>0, 'ips'=>['10.244.2.6']},
      {'job_name'=>'other_job', 'index'=>0, 'ips'=>['10.244.2.254']}]
   }
@@ -19,6 +23,7 @@ describe BindingCommands::Bosh::DeploymentVmState do
     expect(bosh_director_client).to receive(:fetch_vm_state).with(deployment_name).and_return([vms_state, 'some-task-id'])
 
     subject.perform
-    expect(subject.to_json).to eq(vms_state.to_json)
+    expect(subject.vms_state).to eq(expected_vms_state)
+    expect(subject.to_json).to eq(expected_vms_state.to_json)
   end
 end
