d46ab7ddf32c0a8303ac9e6c9b25d6686871c537
Dr Nic Williams <drnicwilliams@gmail.com>
2014-01-02 08:55:02 -0800

    refactor Actions::DeleteBindingCommands

--- app/models/actions/delete_binding_commands.rb
+++ app/models/actions/delete_binding_commands.rb
@@ -4,12 +4,25 @@ class Actions::DeleteBindingCommands
   attr_accessor :service_binding_id
 
   def perform
+    delete_binding_commands
+    delete_list_of_registered_commands
+  rescue Net::HTTPServerException
+    # /registered_binding_commands/#{service_binding_id} key not in etcd
+  end
+
+  private
+  def delete_binding_commands
     registered_binding_commands = JSON.parse($etcd.get("/registered_binding_commands/#{service_binding_id}").value)
     registered_binding_commands.each do |binding_command_uuid|
-      $etcd.delete("/binding_commands/#{binding_command_uuid}")
+      begin
+        $etcd.delete("/binding_commands/#{binding_command_uuid}")
+      rescue Net::HTTPServerException
+        # key not in etcd
+      end
     end
+  end
+
+  def delete_list_of_registered_commands
     $etcd.delete("/registered_binding_commands/#{service_binding_id}")
-  rescue Net::HTTPServerException
-    # key not in etcd
   end
 end
